89dc632f7fc2769a0a48982fd4ebe2149af65e88
Guido Lange <32455006+Gylxan@users.noreply.github.com>
2019-03-09 17:11:51 +0100

    Set default port back to 5000 for dev and initiate DB on start of server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 5000;
 const host = process.env.HOST || '0.0.0.0';
 const apiRoutes = require('./server/routes/index');
 // Start app
